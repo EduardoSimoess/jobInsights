dbe98427ae9b9a22c30ce944537e743ecd896e1d
Eduardo Simões <eduardorsimoes97@gmail.com>
2023-02-23 17:04:54 -0300

    req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
 # from src.pre_built.counter import count_ocurrences
+# import json
+# import pytest
+from src.pre_built.counter import count_ocurrences
 
 
+# @pytest.fixture
 def test_counter():
-    pass
+    assert count_ocurrences('data/jobs.csv', 'industry') == 1346
